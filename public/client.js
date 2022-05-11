6f1aca2028630bdb9b35b3e029ad57962c19f3d3
Zkh-dot <58977268+Zkh-dot@users.noreply.github.com>
2022-05-11 12:19:57 +0300

    version 0.3

--- public/client.js
+++ public/client.js
@@ -1,9 +1,56 @@
-//console.log('Client-side code running');
 
 const button = document.getElementById('myButton');
+const tbutton = document.getElementById('tikerbutton');
  
+var my_div = newDiv = null;
+
+function addElement(id, text) {
+
+  // Создаём новый элемент div
+  // и добавляем в него немного контента
+
+  var newDiv = document.createElement("div");
+      newDiv.innerHTML = '<b><p align="center" id="' + id + '">' + text + '</p></b>';
+
+  // Добавляем только что созданный элемент в дерево DOM
+
+  my_div = document.getElementById("HRP");
+  document.body.insertBefore(newDiv, my_div);
+}
 
 setInterval(function() {
+  fetch('/custom_tikers', {method: 'GET'})
+  .then(function(response) {
+    if(response.ok) return response.json();
+    throw new Error('Request failed.');
+  })
+  .then(function(data) {
+    tikers = data[1];
+    if(tikers != [])
+    {
+      for(i in tikers){
+        /*fetch('/customsale', {  
+          method: 'post',
+          headers: {'Content-Type': 'application/json;charset=utf-8'},
+          body: JSON.stringify(tikers[i]),
+        })
+        .then(function(response) {
+          if(response.ok) alert('Ваши тикеры записаны');
+        })
+        .then(function(data) {*/
+          var element=document.getElementById(tikers[i] + 'tiker');
+          if(!element){
+            addElement(tikers[i] + 'tiker', tikers[i] + ' is now loading')
+          }
+
+          else{
+            document.getElementById(i + 'tiker').innerHTML = `${tikers} is now ${2}`;
+          }
+        //})
+      }
+    }
+  })
+
     fetch('/BTC', {method: 'GET'})
       .then(function(response) {
         if(response.ok) return response.json();
@@ -44,26 +91,34 @@ setInterval(function() {
       
   }, 2000);
 
+tbutton.addEventListener('click', function(e) {
+  console.log(1);
+  fetch('/get_extra', {  
+    method: 'post',
+    headers: {'Content-Type': 'application/json;charset=utf-8'},
+    body: JSON.stringify({1: document.getElementById("tik").value}),
+})
+  .then(function(response) {
+    if(response.ok) alert('Ваши тикеры записаны');
+  })
+})
 
 button.addEventListener('click', function(e) {
-  tgid = document.getElementById("tg").value;
+  //tgid = document.getElementById("tg").value;
   BTC_target= document.getElementById("iBTC").value;
   ETH_target = document.getElementById("iETH").value;
   LTC_target = document.getElementById("iLTC").value;
   var targets = {};
-    if(tgid != ""){
-        if (targets[tgid] == undefined){
-          targets[tgid] = {}
-         }
+         
     
         if(BTC_target != ''){
-          targets[tgid]['BTCUSD'] = BTC_target;
+          targets['BTCUSD'] = BTC_target;
         }
         if(ETH_target != ''){
-          targets[tgid]['ETHUSD'] = ETH_target;
+          targets['ETHUSD'] = ETH_target;
         }
         if(LTC_target != ''){
-          targets[tgid]['LTCUSD'] = LTC_target;
+          targets['LTCUSD'] = LTC_target;
         }
         fetch('/tg', {  
           method: 'post',
@@ -74,19 +129,6 @@ button.addEventListener('click', function(e) {
         if(response.ok) alert('Ваши таргеты записаны');
       })
         
-    }
-    else{
-        ////console.log(document.getElementById("tg").value);
-    }
+    
+    
 });
-
-/*try {
-  const response = await fetch('https://example.com/profile/avatar', {
-    method: 'PUT',
-    body: formData
-  });
-  const result = await response.json();
-  //console.log('Успех:', JSON.stringify(result));
-} catch (error) {
-  console.error('Ошибка:', error);
-}*/
