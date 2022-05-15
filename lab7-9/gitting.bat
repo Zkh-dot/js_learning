3810e9c78fd3891394fb9ed9f8c2c346f677fb3d
Zkh-dot <58977268+Zkh-dot@users.noreply.github.com>
2022-05-15 23:21:03 +0300

    Delete lab7-9 directory

--- lab7-9/gitting.bat
+++
@@ -1,6 +0,0 @@
-del .git
-git init
-git add . 
-git commit -m "First commit Message"
-git config --global user.email "kernzahar@gmail.com"
-git push git@github.com:Zkh-dot/js_learning.git master -u --force
